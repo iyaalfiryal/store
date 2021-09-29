be36469966bb81714b9eea5641099a0bb691a7ae
iyaalfiryal <fifihuwaida@gmail.com>
2021-09-29 20:59:26 +0700

    Initial commit

---
+++ script/navbar-scroll.js
@@ -0,0 +1,7 @@
+// TODO 7 Navigation bar Script
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
